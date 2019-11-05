b063d1888d880b3c6b484d8cdfbeee1cc16a758a
LOXXX <elik@example.com>
2019-11-05 20:30:43 +0300

    upadate hw

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from vk_api.bot_longpoll import VkBotLongPoll
 import vk_api
 from bs4 import BeautifulSoup
-from hw import GetHomework
+from hw import Homework
 
 XML = open('settings.xml', 'r', encoding="utf-8").read()
 SETTINGS = BeautifulSoup(XML, "lxml")
@@ -11,9 +11,20 @@ ID = SETTINGS.find('groupid').text
 vk = vk_api.VkApi(token=TOKEN)
 vk.get_api()
 longpoll = VkBotLongPoll(vk, ID)
+doc = open('doc.txt', 'r', encoding='UTF-8').read()
 
 for event in longpoll.listen():
-    print(event.object.peer_id)
-    if event.object.text.lower() and event.object.text.lower() == "ботдз":
-        vk.method("messages.send", {"peer_id": event.object.peer_id,
-                                    "message": GetHomework().tomorrow(), "random_id": 0})
+    if event.object.text:
+        print(event.object)
+
+        if event.object.text.lower() == 'помощь':
+            vk.method("messages.send",
+                      {"peer_id": event.object.peer_id,
+                       "message": doc, "random_id": 0})
+
+        else:
+            homework = Homework()
+            if event.object.text.lower() == 'ботдз':
+                vk.method("messages.send",
+                          {"peer_id": event.object.peer_id,
+                           "message": homework.get_tomorrow(), "random_id": 0})
--- hw.py
+++ hw.py
@@ -4,8 +4,10 @@ import datetime
 
 XML = open('settings.xml', 'r', encoding="utf-8").read()
 SETTINGS = BeautifulSoup(XML, "lxml").find('settings')
-LOGIN = SETTINGS.find('login').text
-PASSWORD = SETTINGS.find('password').text
+LOGIN_1 = SETTINGS.find('login-1').text
+PASSWORD_1 = SETTINGS.find('password-1').text
+LOGIN_2 = SETTINGS.find('login-2').text
+PASSWORD_2 = SETTINGS.find('password-2').text
 
 
 def get_session(l, p):
@@ -27,41 +29,59 @@ def get_next_day():
     return day
 
 
+SESSION_1 = get_session(LOGIN_1, PASSWORD_1)
+SESSION_2 = get_session(LOGIN_2, PASSWORD_2)
+NEXT_DAY = get_next_day()
+NEXT_DAY_INT = get_next_day().isoweekday()
+
+
+
 def get_day_timetable(day):
     timetable = BeautifulSoup(XML, "lxml").find('timetable')
-    timetable_of_day_raw = ' '.join(timetable.findAll('day')[day].text.split()).split()
-    timetable_of_day = tuple(zip(timetable_of_day_raw[::2], timetable_of_day_raw[1::2]))
-    return timetable_of_day
+    timetable_of_day_raw = timetable.findAll('day')[day].find_all('lesson')
+    timetable_of_day = []
+    for lesson in range(0, len(timetable_of_day_raw)):
+        timetable_of_day.append(tuple(timetable_of_day_raw[lesson].text.split()))
+    return tuple(timetable_of_day)
 
 
-class GetHomework:
+def get_lesson_homework(lsn, ses):
+    subj = lsn[0]
+    link = lsn[1]
+
+    subj_soup = BeautifulSoup(ses.get(link).text, 'html.parser')
+    homework = subj_soup.find('table', class_="subject-stat")
+
+    if homework:
+        homework = ' '.join(subj_soup.find('table', class_="subject-stat").find_all('td')[2].text.split())
+
+    if homework == '' or not homework:
+        homework = 'Нет дз / Учитель его не написал'
+
+    return subj + ' - ' + homework + '\n\n'
+
+
+def get_res(day_int, is_next_day, rslt, ses1, ses2):
+    if is_next_day:
+        for lsn in get_day_timetable(day_int-1):
+            if len(lsn) == 2:
+                rslt += get_lesson_homework(lsn, ses1)
+            else:
+                rslt += get_lesson_homework(lsn[:2:], ses1)
+                rslt += get_lesson_homework(lsn[2::], ses2)
+        return rslt.replace('_', ' ')
+    else:
+        return None
+
+
+class Homework:
     def __init__(self):
-        self.session = get_session(LOGIN, PASSWORD)
+        self.result = 'Домашнее задание на ' + str(NEXT_DAY.strftime('%d.%m.%Y')) + '\n' \
+             + 'Также читайте сайт класса: loxxx.tk ' + '\n\n'
 
-        self.next_day = get_next_day()
-        self.next_day_int = get_next_day().isoweekday()
-
-        self.result = 'Домашнее задание на ' + str(self.next_day.strftime('%d.%m.%Y')) + '\n'\
-                      + 'Также читайте сайт класса: loxxx.tk ' + '\n\n'
-
-    def tomorrow(self):
-        res = self.result
-        for lesson in get_day_timetable(self.next_day_int-1):
-            subj = lesson[0]
-            link = lesson[1]
-
-            subj_soup = BeautifulSoup(self.session.get(link).text, 'html.parser')
-            homework = subj_soup.find('table', class_="subject-stat")
-
-            if homework:
-                homework = ' '.join(subj_soup.find('table', class_="subject-stat").find_all('td')[2].text.split())
-
-            if homework == '' or not homework:
-                homework = 'Нет дз / Учитель его не написал'
-
-            res += subj + ' - ' + homework + '\n'
-        return res
+    def get_tomorrow(self):
+        return get_res(NEXT_DAY_INT, True, self.result, SESSION_1, SESSION_2)
 
 
 if __name__ == '__main__':
-    print(GetHomework().tomorrow())
+    print(Homework().get_tomorrow())
